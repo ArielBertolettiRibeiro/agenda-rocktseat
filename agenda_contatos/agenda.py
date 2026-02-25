188899e4daa02989dfda28b26af3f50402318a62
ArielBertolettiRibeiro <arielbertolettiribeiro@gmail.com>
2026-02-25 13:59:19 -0300

    feat: add initial agenda module structure.

---
+++ agenda_contatos/agenda.py
@@ -0,0 +1,10 @@
+def menu():
+    """
+    1. Adicionar contato.
+    2. Editar contato.
+    3. Marcar contato como favorito.
+    4. Remover contato.
+    
+    Escolhe uma opção:
+    """
+    return
